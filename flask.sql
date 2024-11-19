c1075709aca64c99871802b4e3bec90b5009c33f
Han Yue <alexyuehan@gmail.com>
2024-11-19 14:47:16 -0500

    Init studentweb

---
+++ flask.sql
@@ -0,0 +1,55 @@
+/*
+ Navicat MySQL Dump SQL
+
+ Source Server         : flask
+ Source Server Type    : MySQL
+ Source Server Version : 90100 (9.1.0)
+ Source Host           : localhost:3306
+ Source Schema         : flask
+
+ Target Server Type    : MySQL
+ Target Server Version : 90100 (9.1.0)
+ File Encoding         : 65001
+
+ Date: 19/11/2024 14:37:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `sex` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `age` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (777, 'leona', 'femal', 18);
+INSERT INTO `student` VALUES (6666, 'leo', 'male', 18);
+INSERT INTO `student` VALUES (8888, 'thresh', 'male', 30);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'alex', '123');
+
+SET FOREIGN_KEY_CHECKS = 1;
